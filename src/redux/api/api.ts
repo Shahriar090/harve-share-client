81210bf289dbe5362cea09d31266cfa6f69b3417
Shahriar <shahriarh655@gmail.com>
2024-05-11 16:00:32 +0600

    testing

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,7 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl:
+      "https://harve-share-anqmsnp0d-shahriar090s-projects.vercel.app/api/v1/",
+  }),
   tagTypes: ["supply"],
   endpoints: (builder) => ({
     getSupplies: builder.query({
